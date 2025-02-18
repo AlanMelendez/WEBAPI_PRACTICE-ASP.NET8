9d695d05a1c3c4b84c5fee9ee0b478f8f2ccfead
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-02-17 23:10:30 -0600

    add validation by models

--- web-api/DTOs/Stock/CreateStockRequestDTO.cs
+++ web-api/DTOs/Stock/CreateStockRequestDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,26 @@ namespace web_api.DTOs.Stock
 {
     public class CreateStockRequestDTO
     {
+        [Required]
+        [MinLength(1, ErrorMessage = "Symbol must be at least 1 character long")]
+        [MaxLength(10, ErrorMessage = "Symbol must be at most 50 characters long")]
         public string Symbol {get; set;} = string.Empty;
+        [Required]
+        [MinLength(5, ErrorMessage = "Company Name must be at least 1 character long")]
+        [MaxLength(50, ErrorMessage = "Company Name must be at most 50 characters long")]
         public string CompanyName { get; set; } = string.Empty;
+        [Required]
+        [Range(1,1000000)]
         public decimal Purchase { get; set; }
-
+        [Required]
+        [Range(0.001,100)]
         public decimal LastDiv { get; set; }
+        [Required]
+        [MinLength(10, ErrorMessage = "Industry must be at least 1 character long")]
+        [MaxLength(50, ErrorMessage = "Industry must be at most 50 characters long")]
         public string Industry { get; set; } = string.Empty;
-
+        [Required]
+        [Range(1,5000000000)]
         public long MarketCap { get; set; }
     }
 }
